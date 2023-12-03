4b46c9e14e754d1235988a5c3c6850186b48cf68
ducjk <duchuynh360@gmail.com>
2023-12-03 21:37:32 +0700

    create modal, tab, auth modal and nav component

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  private modals: IModal[] = []
+
+  constructor() { }
+
+  register(id:string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+
+    console.log(this.modals);
+    
+  }
+
+  unregister(id: string){
+    this.modals.filter(e => e.id !== id)
+  }
+
+  isModalOpen(id: string): boolean{
+    return !!this.modals.find(e => e.id === id)?.visible
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(e => e.id === id)
+
+    if (modal){
+      modal.visible = !modal.visible
+    }
+    // this.visible = !this.visible
+
+  }
+}
