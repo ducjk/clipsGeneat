65763e65f84efba6681f859a39d853a9adb2dfe3
ducjk <duchuynh360@gmail.com>
2023-12-08 09:29:11 +0700

    login, register form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  safelist: ['bg-blue-400', 'bg-green-400', 'bg-red-400'],
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    opacity: ({ after }) => after(['disabled']),
+    hover: ({after}) => after(['disabled'])
   },
   plugins: [],
 }
